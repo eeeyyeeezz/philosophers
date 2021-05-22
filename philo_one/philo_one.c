e2c93531c135f9149a16b38b7d257f7c5e3dee89
Georgianne Morra <gmorra@student.21-school.ru>
2021-05-22 15:28:51 +0300

    mutex how to handle??

--- philo_one/philo_one.c
+++ philo_one/philo_one.c
@@ -18,7 +18,7 @@ void		*print(void *buf)
 	return (NULL);
 }
 
-void		create_threads(pthread_t *philo, t_info *info)
+void		create_threads(pthread_t *philo, pthread_mutex_t *forks, t_info *info)
 {
 	int		i;
 
@@ -30,7 +30,8 @@ void		create_threads(pthread_t *philo, t_info *info)
 
 int			main(int argc, char **argv)
 {	
-	pthread_t	*philo;
+	pthread_t		*philo;
+	pthread_mutex_t	*forks;
 	t_info		info;
 
 	check_errors(argc, argv);
@@ -38,8 +39,11 @@ int			main(int argc, char **argv)
 	pars_arg(&info, argc, argv);
 	philo = malloc(sizeof(pthread_t) * (info.philo_num + 1));
 	if (!philo)
-		return (1);
-	create_threads(philo, &info);
+		return (ft_error("Malloc Error!\n"));
+	forks = malloc(sizeof(pthread_mutex_t) * info.philo_num);
+	if (!forks)
+		return (ft_error("Malloc Error!\n"));
+	create_threads(philo, forks, &info);
 
 	// printf(")) [%d] [%d] [%d] [%d]\n", info.philo_num, info.time_live, info.time_eat, info.time_sleep);
 }

f370214eb07b93bd75e2dcdb44cdfa88fcda19c5
Georgianne Morra <gmorra@student.21-school.ru>
2021-05-25 17:16:53 +0300

    almost done philo_one

--- philo_one/philo_one.c
+++ philo_one/philo_one.c
@@ -53,16 +53,20 @@ void	lock_right_mutex(t_state *state)
 
 void		check_dead(t_state *state)
 {
-	// printf("TIME LIVE [%d] gettime [%lu] philotime [%zd]\n", state->time_live, get_time(*state->time), state->philo_time);
+	// printf("TIME LIVE [%d] gettime [%lu] philotime [%zd] philo [%d]\n", state->time_live, get_time(*state->time), state->philo_time, state->philo_score);
 	if ((state->time_live - (get_time(*state->time) - state->philo_time)) <= 0)
-			printf("IM DEAD BRO [%d]\n", state->philo_score);
+	{
+		pthread_mutex_lock(state->write);	
+		printf("\033[0;35m[%zd]\033[0m %d \033[1;31mis dead\033[0m\n", get_time(*state->time), state->philo_score);
+		pthread_mutex_unlock(state->write);	
+		exit(1);
+	}
 }
-	// putnbr_str(state->philo_score, " Has taken a fork\n");
-	// putnbr_str(state->philo_score, " Has taken a fork\n");
-	// putnbr_str(state->philo_score, " Is eating\n");
 
 void		philo_eat(t_state *state)
 {
+	// if (state->philo_time == 0)
+	// 	check_dead(state);
 	lock_left_mutex(state);
 	printf("\033[0;35m[%lu]\033[0m %d \033[0;32mhas taken a fork\033[0m\n", get_time(*state->time), state->philo_score);
 	lock_right_mutex(state);
@@ -75,14 +79,13 @@ void		philo_eat(t_state *state)
 
 void		philo_sleep(t_state *state)
 {
-	printf("\033[0;35m[%lu]\033[0m %d \033[0;31mis sleping\033[0m\n", get_time(*state->time), state->philo_score);
+	printf("\033[0;35m[%lu]\033[0m %d \033[0;33mis sleeping\033[0m\n", get_time(*state->time), state->philo_score);
 	ft_usleep(state->time_sleep);
 }
 
 void		philo_think(t_state *state)
 {
 	printf("\033[0;35m[%lu]\033[0m %d \033[1;37mis thinking\033[0m\n", get_time(*state->time), state->philo_score);
-	
 }
 
 void		*start_eat(void *tmp_state)
@@ -96,31 +99,50 @@ void		*start_eat(void *tmp_state)
 	else	
 		count = state->times_to_eat;
 
-	// state->philo_time = state->time_eat;
 	while (count--)
 	{
 		philo_eat(state);
-		check_dead(state);
-		// printf("TIME LIVE [%d] gettime [%lu] philotime [%zd]\n", state->time_live, get_time(*state->time), state->philo_time);
-		// if ((state->time_live - (get_time(*state->time) - state->philo_time)) <= 0)
-		// 	printf("IM DEAD BRO\n");
 		philo_sleep(state);
 		philo_think(state);
 	}
 	return (NULL);
 }
 
+void		*dead_thread(void *tmp_state)
+{
+	t_struct *global;
+	int i;
+	global = (t_struct *)tmp_state;
+
+	while (1)
+	{
+		usleep(global->state[0].time_live / 4);
+		i = -1;
+		// printf("INFO %d\n", state->philo_score);
+		while (++i < global->philo_num)
+		{
+			if ((global->state[i].time_live - (get_time(*global->state[i].time) - global->state[i].philo_time)) <= 0 && global->state[i].philo_time != 0)
+			{
+				pthread_mutex_lock(global->state[i].write);	
+				printf("\033[0;35m[%zd]\033[0m %d \033[1;31mis dead\033[0m\n", get_time(*global->state[i].time), global->state[i].philo_score);
+				return (NULL);
+			}
+		}
+		
+	}
+	return (NULL);
+}
+
 void		pthreads_create(t_struct *global, pthread_t *philo, int argc)
 {
-	int i;
+	int			i;
+	pthread_t	philo_dead;
 
 	i = -1;
+	pthread_create(&philo_dead, NULL, dead_thread, (void *)global);
 	while (++i < global->philo_num)
-	{
-		// write(1, "kavo\n", 5);
 		pthread_create(&philo[i], NULL, start_eat, (void *)&global->state[i]);
-		// pthread_join(philo[i], (void *)&global->state[i]);
-	}
+	pthread_join(philo_dead, NULL);
 }
 
 int			main(int argc, char **argv)
@@ -130,7 +152,7 @@ int			main(int argc, char **argv)
 
 	global.time = get_time(0);
 	check_errors(argc, argv);
-	global.state = malloc(sizeof(t_state) * ft_atoi(argv[1]));
+	global.state = malloc(sizeof(t_state) * (ft_atoi(argv[1]) + 1));
 	if (!global.state)
 		return (ft_error("Malloc Error!\n"));
 	declare_struct(&global, argv, argc);
@@ -139,8 +161,15 @@ int			main(int argc, char **argv)
 	if (!philo)
 		return (ft_error("Malloc Error!\n"));
 	pthreads_create(&global, philo, argc);
-	while (1){}
+	return (0);
+	// while (1)
+	// {
+	// 	// check_dead(global.state);
+	// 	// if (global.state->philo_dead)
+	// 		// exit(1);
+	// }
 }
 
 // !) ft_error освобождение памяти добавить
 // Защищать вывод мьютексом? 
+// Когда все поели выйти из программы

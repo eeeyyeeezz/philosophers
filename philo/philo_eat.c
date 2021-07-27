7926bcfa91d8f4b2722253b9273358c472a58096
eeeyyeeezz <eximmlich@gmail.com>
2021-07-27 15:37:13 +0300

    fixes with lots of threads + norme

--- philo/philo_eat.c
+++ philo/philo_eat.c
@@ -10,21 +10,37 @@
 void	take_even_forks(t_state *state)
 {
 	if (state->philo_score % 2 == 0)
+	{
+		pthread_mutex_lock(state->write);
 		printf("\033[0;35m[%lu]\033[0m %d \033[0;32mhas taken a L fork\033[0m\n",
 			get_time(*state->time), state->philo_score);
+		pthread_mutex_unlock(state->write);
+	}
 	else
+	{
+		pthread_mutex_lock(state->write);
 		printf("\033[0;35m[%lu]\033[0m %d \033[0;32mhas taken a R fork\033[0m\n",
 			get_time(*state->time), state->philo_score);
+		pthread_mutex_unlock(state->write);
+	}
 }
 
 void	take_odd_forks(t_state *state)
 {
 	if (state->philo_score % 2 == 0)
+	{
+		pthread_mutex_lock(state->write);
 		printf("\033[0;35m[%lu]\033[0m %d \033[0;32mhas taken a R fork\033[0m\n",
 			get_time(*state->time), state->philo_score);
+		pthread_mutex_unlock(state->write);
+	}
 	else
+	{
+		pthread_mutex_lock(state->write);
 		printf("\033[0;35m[%lu]\033[0m %d \033[0;32mhas taken a L fork\033[0m\n",
 			get_time(*state->time), state->philo_score);
+		pthread_mutex_unlock(state->write);
+	}
 }
 
 void	philo_eat(t_state *state)
@@ -34,16 +50,20 @@ void	philo_eat(t_state *state)
 	lock_odd_mutex(state);
 	take_odd_forks(state);
 	state->philo_time = get_time(*state->time);
+	pthread_mutex_lock(state->write);
 	printf("\033[0;35m[%lu]\033[0m %d \033[0;34mis eating\033[0m\n",
 		get_time(*state->time), state->philo_score);
+	pthread_mutex_unlock(state->write);
 	ft_usleep(state->time_eat);
 	unlock_mutex(state);
 }
 
 void	philo_sleep(t_state *state)
 {
+	pthread_mutex_lock(state->write);
 	printf("\033[0;35m[%lu]\033[0m %d \033[0;33mis sleeping\033[0m\n",
 		get_time(*state->time), state->philo_score);
+	pthread_mutex_unlock(state->write);
 	ft_usleep(state->time_sleep);
 }
 
@@ -61,12 +81,16 @@ void	*start_eat(void *tmp_state)
 	{
 		philo_eat(state);
 		philo_sleep(state);
+		pthread_mutex_lock(state->write);
 		printf("\033[0;35m[%lu]\033[0m %d \033[1;37mis thinking\033[0m\n",
 			get_time(*state->time), state->philo_score);
+		pthread_mutex_unlock(state->write);
 	}
 	if (state->done_eat != -1)
 		state->done_eat = 1;
+	pthread_mutex_lock(state->write);
 	printf("\033[0;35m[%lu]\033[0m %d \033[2;37mhas finished eating!\033[0m\n",
 		get_time(*state->time), state->philo_score);
+	pthread_mutex_unlock(state->write);
 	return (NULL);
 }
